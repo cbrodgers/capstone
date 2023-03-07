54f4a2c9f57abb23a9e2eed61ae1565a022a2ff2
cbrodgers <cbrodgers@ualr.edu>
2023-03-07 02:08:16 +0000

    work

---
+++ assets/scripts/script.js
@@ -0,0 +1,24 @@
+function myFunction() {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+}
+
+document.getElementById("defaultOpen").click();
+
+function openTab(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+  
+function openRedirect(url) {
+  window.location.href = url;
+}
