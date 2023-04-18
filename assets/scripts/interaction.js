1cf6832d974bbad40d0cdaea1d4ccca86f76486b
cbrodgers <cbrodgers@ualr.edu>
2023-04-18 21:57:24 +0000

    new

--- assets/scripts/interaction.js
+++ assets/scripts/interaction.js
@@ -6,7 +6,7 @@ async function getName() {
 
     if (form.valid()) {
         
-        var search_name = document.getElementById("search").value.toUpperCase();
+        var search_name = document.getElementById("search").value.toLowerCase();
 
         var nameURL = "https://rxnav.nlm.nih.gov/REST/RxTerms/allconcepts.json";
         
@@ -17,11 +17,11 @@ async function getName() {
         var y = JSON.parse(nameJSONText);
         var foundname = 0;
         for( var i = 0; i < y.minConceptGroup.minConcept.length; i++ ){
-            var fn = y.minConceptGroup.minConcept[i].fullName.toUpperCase();
+            var fn = y.minConceptGroup.minConcept[i].fullName.toLowerCase();
             if (fn.includes(search_name) == true) {
                 foundname = i;
                 var myrxcui = y.minConceptGroup.minConcept[i].rxcui;
-                document.getElementById("results").innerHTML = "Name: " + foundname + ". Rxcui:" + myrxcui;
+                document.getElementById("results").innerHTML = "Name: " + search_name + "," + foundname + ". Rxcui:" + myrxcui;
                 getData(myrxcui);
             }
 
@@ -35,4 +35,17 @@ async function getName() {
 async function getData(rxcui) {
     "use strict";
     var x = rxcui
+
+    var reactionURL = "https://rxnav.nlm.nih.gov/REST/interaction/interaction.json?rxcui=" + rxcui + "&sources=ONCHigh";
+
+    var interactionObject = await fetch(reactionURL);
+
+    var reactionJSONText = await interactionObject.text();
+
+    var r = JSON.parse(reactionJSONText);
+
+    var int = r.interactionTypeGroup.interactionPair.description;
+    
+    
+    document.getElementById("interaction").innerHTML = int;
 }
